e373a8aa060a5c8034d76405f01856d1b47083f8
Giulia Ghisini <giulia.ghisini@redturtle.it>
2024-06-07 14:45:29 +0200

    feat: first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+export FeedbackForm from 'volto-feedback-italia/components/FeedbackForm/FeedbackForm';
+export AnswersStep from 'volto-feedback-italia/components/FeedbackForm/Steps/AnswersStep';
+export CommentsStep from 'volto-feedback-italia/components/FeedbackForm/Steps/CommentsStep';
+export Rating from 'volto-feedback-italia/components/FeedbackForm/Steps/Commons/Rating';
+
+const applyConfig = (config) => {
+  return config;
+};
+
+export default applyConfig;
